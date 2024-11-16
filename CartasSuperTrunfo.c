d5f6ad9226d2abfa2d95194783d59e317bf1a24f
Ruann-Monteiro <RUANNMONTEIRO10@GMAIL.COM>
2024-11-16 19:17:32 +0000

    Adição da terceira carta e finalização.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,7 @@ int main() {
      int codigo_da_cidade;
      int populacao;
      float PIB;
+     int area;
      int numero_de_pontos_turisticos;
      char nome[50];
 
@@ -22,8 +23,18 @@ int main() {
      int codigo_da_cidade2;
      int populacao2;
      float PIB2;
+     int area2;
      int numero_de_pontos_turisticos2;
      char nome2[50];
+
+//Terceira carta
+
+     int codigo_da_cidade3;
+     int populacao3;
+     float PIB3;
+     int area3;
+     int numero_de_pontos_turisticos3;
+     char nome3[50];
 
     // Cadastro
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.ro das Cartas:
@@ -31,23 +42,30 @@ int main() {
 
 // Primeira carta
 
+    printf("Nome da Cidade: \n");
+    scanf("%s", &nome);
+
     printf("Codigo da Cidade: \n");
     scanf("%d", &codigo_da_cidade);
 
     printf("População: \n");
     scanf("%d", &populacao);
 
+    printf("Area da cidade: \n");
+    scanf("%d", &area);
+
     printf("PIB:$ \n");
     scanf("%f", &PIB);
 
     printf("Numero de pontos turisticos: \n");
     scanf("%d", &numero_de_pontos_turisticos);
-
-    printf("Nome da Cidade: \n");
-    scanf("%s", &nome);
+ 
     printf("\n");
 
 //Segunda carta 
+
+    printf("Nome da Cidade: \n");
+    scanf("%s", &nome2);
 
     printf("Codigo da Cidade: \n");
     scanf("%d", &codigo_da_cidade2);
@@ -55,14 +73,37 @@ int main() {
     printf("População: \n");
     scanf("%d", &populacao2);
 
+    printf("Area da cidade: \n");
+    scanf("%d", &area2);
+
     printf("PIB:$ \n");
     scanf("%f", &PIB2);
 
     printf("Numero de pontos turisticos: \n");
     scanf("%d", &numero_de_pontos_turisticos2);
 
+    printf("\n");
+
+//Terceira carta
+
     printf("Nome da Cidade: \n");
-    scanf("%s", &nome2);
+    scanf("%s", &nome3);
+
+    printf("Codigo da Cidade: \n");
+    scanf("%d", &codigo_da_cidade3);
+
+    printf("População: \n");
+    scanf("%d", &populacao3);
+
+    printf("Area da cidade: \n");
+    scanf("%d", &area3);
+
+    printf("PIB:$ \n");
+    scanf("%f", &PIB3);
+
+    printf("Numero de pontos turisticos: \n");
+    scanf("%d", &numero_de_pontos_turisticos3);
+
     printf("\n");
 
     // Exibição dos Dados das Cartas:
@@ -74,6 +115,7 @@ int main() {
     printf("Nome da Cidade: %s\n", nome);
     printf("Codigo da Cidade: %d\n",codigo_da_cidade);
     printf("População: %d\n",populacao);
+    printf("Área: %d\n",area);
     printf("PIB: $%.3f\n",PIB);
     printf("Numero de pontos turisticos: %d\n",numero_de_pontos_turisticos);
     printf("\n");
@@ -83,9 +125,20 @@ int main() {
     printf("Nome da Cidade: %s\n", nome2);
     printf("Codigo da Cidade: %d\n",codigo_da_cidade2);
     printf("População: %d\n",populacao2);
+     printf("Área: %d\n",area2);
     printf("PIB: $%.3f\n",PIB2);
     printf("Numero de pontos turisticos: %d\n",numero_de_pontos_turisticos2);
     printf("\n");
  
+ //Terceira carta
+
+    printf("Nome da Cidade: %s\n", nome3);
+    printf("Codigo da Cidade: %d\n",codigo_da_cidade3);
+    printf("População: %d\n",populacao3);
+     printf("Área: %d\n",area3);
+    printf("PIB: $%.3f\n",PIB3);
+    printf("Numero de pontos turisticos: %d\n",numero_de_pontos_turisticos3);
+    printf("\n");
+
     return 0;
 }
